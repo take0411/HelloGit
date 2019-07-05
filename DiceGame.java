e1dc4b33b18304172a377c9b9cff79a26d6186f6
崎田健史 <sakita@wlan-napt-003.u-aizu.ac.jp>
2019-07-05 20:21:28 +0900

    add new file

---
+++ DiceGame.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] args) {
+	String name;
+	int d1,d2;
+	Random r=new Random();
+
+	d1=r.nextInt(6)+1;
+	d2=r.nextInt(6)+1;
+
+        System.out.println("Rolling the dice...");
+
+        System.out.println("Die 1:"+d1);
+        System.out.println("Die 2:"+d2);
+        System.out.println("Total value:"+(d1+d2));
+
+    }
+}
